2e7fcddb0f550863da79e9eb153c5e18cb2730a0
Roman Kurbatov <roman.kurbatov.1992@gmail.com>
2014-02-11 18:08:09 +0400

    changed year in copyright notices

--- trikGui/motorLever.cpp
+++ trikGui/motorLever.cpp
@@ -1,4 +1,4 @@
-/* Copyright 2013 Roman Kurbatov
+/* Copyright 2014 Roman Kurbatov
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
