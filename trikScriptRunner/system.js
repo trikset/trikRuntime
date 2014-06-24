f7805556ae80f40d73a365e85ee5d2aa7ae29941
Dmitry Mordvinov <dvvsrd@gmail.com>
2014-06-24 15:32:09 +0400

    Added String.format function into qts interpreter

--- trikScriptRunner/system.js
+++ trikScriptRunner/system.js
@@ -7,3 +7,17 @@ var KeysEnum = {
     Power : 116,
     Menu : 139
 };
+
+// String.format function implementation.
+// Taken from http://stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) {
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
+}
