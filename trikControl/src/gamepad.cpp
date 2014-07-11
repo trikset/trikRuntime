2467e5030ddf19682f7a2e7bc0bc47835bcb360e
Yurii Litvinov <yurii.litvinov@gmail.com>
2014-07-11 15:46:42 +0400

    Added missing license header

--- trikControl/src/gamepad.cpp
+++ trikControl/src/gamepad.cpp
@@ -1,3 +1,17 @@
+/* Copyright 2013 - 2014 Matvey Bryksin, Yurii Litvinov, CyberTech Labs Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. */
+
 #include "gamepad.h"
 
 #include <QtCore/QStringList>
@@ -38,7 +52,6 @@ bool Gamepad::isPadPressed(int pad)
 int Gamepad::padX(int pad)
 {
 	return (!mPads.contains(pad) || !mPads.value(pad).isPressed) ? -1 : mPads.value(pad).x;
-
 }
 
 int Gamepad::padY(int pad)
