e5661654b64a238b1a31276ce850313230599dd3
Rostislav Varzar <vrxfile@mail.ru>
2015-02-12 19:14:04 +0300

    Added defines for devices addresses

--- trikControl/include/trikControl/usbMSP430Interface.h
+++ trikControl/include/trikControl/usbMSP430Interface.h
@@ -8,7 +8,24 @@
 #ifndef USBMSP430INTERFACE_H
 #define USBMSP430INTERFACE_H
 
-void privet();
+#define MOT1    0x14
+#define MOT2    0x15
+#define MOT3    0x17
+#define MOT4    0x16
+#define BATT    0x26
+#define SENS1   0x25
+#define SENS2   0x24
+#define SENS3   0x23
+#define SENS4   0x22
+#define SENS5   0x21
+#define SENS6   0x20
+#define ENC1    0x30
+#define ENC2    0x31
+#define ENC3    0x32
+#define ENC4    0x33
+
+
+
 
 #endif // USBMSP430INTERFACE_H
 
