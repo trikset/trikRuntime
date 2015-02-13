22e8ba9293ffbe8336ff0753958c6ac06e60ce12
Rostislav Varzar <vrxfile@mail.ru>
2015-02-13 14:41:38 +0300

    Added decoder function prototype and error defines

--- trikControl/src/usbMSP430Interface.cpp
+++ trikControl/src/usbMSP430Interface.cpp
@@ -7,6 +7,7 @@
 
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 #include <QByteArray>
 #include <QDebug>
 #include "usbMSP430Interface.h"
@@ -15,17 +16,25 @@
 void makeWriteRegPacket(char *msp_packet, uint8_t dev_addr, uint8_t reg_addr, uint32_t reg_val)
 {
     uint8_t crc = (0xFF - (dev_addr + WRITE_FUNC + reg_addr + uint8_t(reg_val & 0xFF) + uint8_t((reg_val >> 8) & 0xFF) +
-                   uint8_t((reg_val >> 16) & 0xFF) + uint8_t((reg_val >> 24) & 0xFF)) + 1) & 0xFF; //Checksum
+                   uint8_t((reg_val >> 16) & 0xFF) + uint8_t((reg_val >> 24) & 0xFF)) + 1) & 0xFF;  //Checksum
     sprintf(msp_packet, ":%02X%02X%02X%08X%02X\n", dev_addr, WRITE_FUNC, reg_addr, reg_val, crc);
 }
 
 /// Make read register packet
 void makeReadRegPacket(char *msp_packet, uint8_t dev_addr, uint8_t reg_addr)
 {
-    uint8_t crc = (0xFF - (dev_addr + READ_FUNC + reg_addr) + 1) & 0xFF; //Checksum
+    uint8_t crc = (0xFF - (dev_addr + READ_FUNC + reg_addr) + 1) & 0xFF;  //Checksum
     sprintf(msp_packet, ":%02X%02X%02X%02X\n", dev_addr, READ_FUNC, reg_addr, crc);
 }
 
+/// Function for decoding received packet
+uint32_t decodeReceivedPacket(char *msp_packet, uint8_t &dev_addr, uint8_t &func_code, uint8_t &reg_addr, uint32_t &reg_val)
+{
+    if (msp_packet[0]!=':')   // Start condition error
+        return START_ERROR;
+    if ((strlen(msp_packet)!=9) && (strlen(msp_packet)!=17))   // Incorrect packet length
+        return LENGTH_ERROR;
+}
 
 /// Write data to MSP430 via USB
 void usb_msp_write(QByteArray const &i2c_data)
--- trikControl/src/usbMSP430Interface.h
+++ trikControl/src/usbMSP430Interface.h
@@ -30,11 +30,28 @@
 #define WRITE_FUNC          0x03
 #define READ_FUNC           0x05
 
+//Error defines
+#define NO_ERROR            0x00
+#define FUNC_CODE_ERROR     0x01
+#define REG_ADDR_ERROR      0x02
+#define REG_VAL_ERROR       0x03
+#define SLAVE_ERROR         0x04
+#define SLAVE_BUSY          0x06
+#define DEV_ADDR_ERROR      0x11
+#define CRC_ERROR           0x12
+#define START_ERROR         0x13
+#define LENGTH_ERROR        0x14
+#define REG_INC_ERROR       0x15
+#define UNDEF_ERROR         0xFF
+
 /// Make write register packet
 void makeWriteRegPacket(char *msp_packet, uint8_t dev_addr, uint8_t reg_addr, uint32_t reg_val);
 
 /// Make read register packet
 void makeReadRegPacket(char *msp_packet, uint8_t dev_addr, uint8_t reg_addr);
+
+/// Function for decoding received packet
+uint32_t decodeReceivedPacket(char *msp_packet, uint8_t &dev_addr, uint8_t &func_code, uint8_t &reg_addr, uint32_t &reg_val);
 
 /// Write data to MSP430 via USB
 void usb_msp_write(QByteArray const &i2c_data);
