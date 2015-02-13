96d1ed9a904852bf3481264e4d54e8fa2b35d1cf
Rostislav Varzar <vrxfile@mail.ru>
2015-02-13 12:59:14 +0300

    Added num to hex functions

--- trikControl/src/usbMSP430Interface.cpp
+++ trikControl/src/usbMSP430Interface.cpp
@@ -9,6 +9,23 @@
 #include <QByteArray>
 #include "usbMSP430Interface.h"
 
+/// Converts number to ASCII code according to HEX format
+uint8_t to_hex(uint8_t i)
+{
+    return (i <= 9 ? '0' + i : 'A' - 10 + i);
+}
+
+/// Converts number to HEX string
+void num2hex(char *string, uint8_t number)
+{
+    string[0] = to_hex((number & 0x00F0) >> 4);
+    string[1] = to_hex(number & 0x000F);
+    string[2] = '\0';
+}
+
+
+
+/// Write data to MSP430 via USB
 void usb_msp_write(QByteArray const &i2c_data)
 {
     if (i2c_data.size() == 2)
@@ -28,6 +45,7 @@ void usb_msp_write(QByteArray const &i2c_data)
     }
 }
 
+/// Read data from MSP430 via USB
 int32_t usb_msp_read(QByteArray const &i2c_data)
 {
     if (i2c_data.size() == 2)
