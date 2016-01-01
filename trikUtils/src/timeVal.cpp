7769bd44463a9a0a19a92526702076c8d8414d14
Korniluk13 <korniluk13@mail.ru>
2016-01-01 15:06:21 +0300

    license info

--- trikUtils/src/timeVal.cpp
+++ trikUtils/src/timeVal.cpp
@@ -1,3 +1,17 @@
+/* Copyright 2015 CyberTech Labs Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. */
+
 #include "timeVal.h"
 
 trikUtils::TimeVal::TimeVal() :
@@ -8,7 +22,6 @@ trikUtils::TimeVal::TimeVal() :
 trikUtils::TimeVal::TimeVal(const TimeVal &timeVal) :
 	mTime(timeVal.mTime)
 {
-
 }
 
 trikUtils::TimeVal::TimeVal(int sec, int mcsec)
