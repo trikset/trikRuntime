9808b39144f15e9eef349962c7fff282127b72dd
greg <zimin_grigory@hotmail.com>
2016-05-24 18:20:11 +0300

    msg from mailbox
    Signed-off-by: greg <zimin_grigory@hotmail.com>

--- trikScriptRunner/src/trikScriptRunner.cpp
+++ trikScriptRunner/src/trikScriptRunner.cpp
@@ -34,6 +34,7 @@ TrikScriptRunner::TrikScriptRunner(trikControl::BrickInterface &brick
 {
 	connect(&mWorkerThread, SIGNAL(finished()), mScriptEngineWorker, SLOT(deleteLater()));
 	connect(&mWorkerThread, SIGNAL(finished()), &mWorkerThread, SLOT(deleteLater()));
+	connect(mailbox, SIGNAL(newMessage(int, QString)), this, SLOT(sendMessageFromMailBox(int, QString)));
 
 	mScriptEngineWorker->moveToThread(&mWorkerThread);
 
@@ -100,4 +101,13 @@ void TrikScriptRunner::onScriptStart(int scriptId)
 	} else {
 		emit startedDirectScript(scriptId);
 	}
+}
+
+
+void TrikScriptRunner::sendMessageFromMailBox(int senderNumber, const QString &message)
+{
+	emit sendMessage(QString("mail: sender: %1 contents: %2")
+			.arg(QString::number(senderNumber))
+			.arg(message)
+	);
 }
