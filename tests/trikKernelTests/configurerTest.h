b8b9753eb4ca3dae5ccac5a5f4c5238d0b7d5ace
kverty <danila_yumsh@mail.ru>
2016-07-25 17:16:29 +0300

    не туда вставил копирайт

--- tests/trikKernelTests/configurerTest.h
+++ tests/trikKernelTests/configurerTest.h
@@ -1,6 +1,3 @@
-#pragma once
-
-#include <gtest/gtest.h>
 /* Copyright 2015 CyberTech Labs Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,6 +11,10 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License. */
+
+#pragma once
+
+#include <gtest/gtest.h>
 
 #include <trikKernel/configurer.h>
 
