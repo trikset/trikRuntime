0826f522ee30d09a2ec52e86acc39271ed2f4328
iakov <jake.kirilenko@gmail.com>
2017-03-21 08:54:20 +0300

    Add doccomment to powerMotor/forceBreak

--- trikControl/src/powerMotor.h
+++ trikControl/src/powerMotor.h
@@ -63,6 +63,7 @@ public slots:
 	/// Period of pulses is a time interval between two characteristic points of two adjacent pulses.
 	void setPeriod(int period);
 
+	/// Forced break for a short period of time followed by power off
 	void forceBreak(int durationMs = 300);
 
 private:
