05238cb7f0c5a65a8a884e55283253c9386d1f38
Yurii Kostyukov <columpio@inbox.ru>
2017-05-30 20:45:20 +0300

    move SIGNALS to interface

--- trikScriptRunner/include/trikScriptRunner/trikScriptRunner.h
+++ trikScriptRunner/include/trikScriptRunner/trikScriptRunner.h
@@ -56,32 +56,6 @@ public slots:
 	void abort() override;
 	void brickBeep() override;
 
-signals:
-	/// Emitted when current script completes execution (for event-driven mode it means that script requested to quit
-	/// or was aborted).
-	/// @param error - localized error message if any error occured during script execution or empty string
-	/// if everything is fine.
-	/// @param scriptId - unique identifier of a script completed
-	void completed(const QString &error, int scriptId);
-
-	/// Emitted when new script from file started.
-	/// @param fileName - name of a file from where the script was loaded.
-	/// @param scriptId - unique id of executed script assigned when script started.
-	void startedScript(const QString &fileName, int scriptId);
-
-	/// Emitted when new direct script started.
-	/// @param scriptId - unique id of executed script assigned when script started.
-	void startedDirectScript(int scriptId);
-
-	/// Emitted when a message must be sent to a desktop.
-	void sendMessage(const QString &text);
-
-private slots:
-	void onScriptStart(int scriptId);
-
-	/// Sends message to host machine from mailbox via wifi.
-	void sendMessageFromMailBox(int senderNumber, const QString &message);
-
 private:
 	QScopedPointer<ScriptExecutionControl> mScriptController;
 
--- trikScriptRunner/include/trikScriptRunner/trikScriptRunnerInterface.h
+++ trikScriptRunner/include/trikScriptRunner/trikScriptRunnerInterface.h
@@ -62,6 +62,26 @@ public slots:
 
 	/// Plays "beep" sound.
 	virtual void brickBeep() = 0;
+
+signals:
+	/// Emitted when current script completes execution (for event-driven mode it means that script requested to quit
+	/// or was aborted).
+	/// @param error - localized error message if any error occured during script execution or empty string
+	/// if everything is fine.
+	/// @param scriptId - unique identifier of a script completed
+	void completed(const QString &error, int scriptId);
+
+	/// Emitted when new script from file started.
+	/// @param fileName - name of a file from where the script was loaded.
+	/// @param scriptId - unique id of executed script assigned when script started.
+	void startedScript(const QString &fileName, int scriptId);
+
+	/// Emitted when new direct script started.
+	/// @param scriptId - unique id of executed script assigned when script started.
+	void startedDirectScript(int scriptId);
+
+	/// Emitted when a message must be sent to a desktop.
+	void sendMessage(const QString &text);
 };
 
 }
