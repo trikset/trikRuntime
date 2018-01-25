125ab9b3b6f1f38fc331ea0e167908717f8668a9
Iakov 'Jake' Kirilenko <Jake.Kirilenko@gmail.com>
2018-01-25 19:15:58 +0300

    getPhoto: save shot to photo.jpg + fixed yuv422p conversion

--- trikControl/src/v4l2CameraImplementation.cpp
+++ trikControl/src/v4l2CameraImplementation.cpp
@@ -1,5 +1,6 @@
+#include <QImage>
 #include "v4l2CameraImplementation.h"
-
+#include "QsLog.h"
 using namespace trikControl;
 
 V4l2CameraImplementation::V4l2CameraImplementation(const QString &port, trikHal::HardwareAbstractionInterface &hardwareAbstraction)
@@ -10,5 +11,14 @@ V4l2CameraImplementation::V4l2CameraImplementation(const QString &port, trikHal:
 
 QVector<uint8_t> V4l2CameraImplementation::getPhoto()
 {
-	return mHal.captureV4l2StillImage(mPort, getTempDir());
+	auto result = mHal.captureV4l2StillImage(mPort, getTempDir());
+	if (result.empty())
+		return result;
+
+	auto image = QImage(result.data(), 320, 240, QImage::Format_RGB888);
+	auto saved = image.save(getTempDir()+"/photo.jpg", "JPG");
+	if (!saved) {
+		QLOG_WARN() << "Failed to save captured image";
+	}
+	return result;
 }
