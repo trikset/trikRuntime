98c40a64a20f612f136b541f9e98925c48eafaa1
Ivan Tyulyandin <ivan.tyulyandin@gmail.com>
2018-07-10 00:00:14 +0300

    V4l2 can find appropriate pixel format and convert it to RGB888, have support for YUV422p and YUV422(YUYV)

--- trikControl/src/v4l2CameraImplementation.cpp
+++ trikControl/src/v4l2CameraImplementation.cpp
@@ -1,9 +1,26 @@
-#include <QImage>
+/* Copyright 2018 Ivan Tyulyandin and CyberTech Labs Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. */
+
 #include "v4l2CameraImplementation.h"
-#include "QsLog.h"
+
+#include <QtGui/QImage>
+#include <QsLog.h>
+
 using namespace trikControl;
 
-V4l2CameraImplementation::V4l2CameraImplementation(const QString &port, trikHal::HardwareAbstractionInterface &hardwareAbstraction)
+V4l2CameraImplementation::V4l2CameraImplementation(const QString &port
+		, trikHal::HardwareAbstractionInterface &hardwareAbstraction)
 	: mHal(hardwareAbstraction)
 	, mPort(port)
 {
@@ -20,5 +37,6 @@ QVector<uint8_t> V4l2CameraImplementation::getPhoto()
 	if (!saved) {
 		QLOG_WARN() << "Failed to save captured image";
 	}
+
 	return result;
 }
--- trikControl/src/v4l2CameraImplementation.h
+++ trikControl/src/v4l2CameraImplementation.h
@@ -1,3 +1,17 @@
+/* Copyright 2018 Ivan Tyulyandin and CyberTech Labs Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. */
+
 #pragma once
 
 #include "cameraImplementationInterface.h"
@@ -11,14 +25,19 @@ class HardwareAbstractionInterface;
 
 namespace trikControl {
 
+/// Camera implementation using v4l2
 class V4l2CameraImplementation : public CameraImplementationInterface
 {
 public:
+
+	/// V4l2 camera constructor
+	/// @param port - name of device, i.e. "/dev/video0"
+	/// @param hardwareAbstraction - realization of HAL
 	V4l2CameraImplementation(const QString &port, trikHal::HardwareAbstractionInterface &hardwareAbstraction);
 
-	QVector<uint8_t> getPhoto();
+	QVector<uint8_t> getPhoto() override;
 
-	~V4l2CameraImplementation() = default;
+	~V4l2CameraImplementation() override = default;
 private:
 	trikHal::HardwareAbstractionInterface & mHal;
 	QString mPort;
