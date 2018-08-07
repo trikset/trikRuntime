02aa8f0c3c119f9b38798abc5a55bee5c0b899c5
Iakov 'Jake' Kirilenko <Jake.Kirilenko@gmail.com>
2018-08-07 17:35:46 +0300

    Code cleanup. Vera++ is OK!

--- pch.h
+++ pch.h
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License. */
 
+#pragma once
 #include <QtCore/QString>
 #include <QtCore/QObject>
 #include <QtCore/QScopedPointer>
