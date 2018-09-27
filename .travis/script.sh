39b1b058a638ffd726e02af5763834d0a67084f2
Iakov Kirilenko <Iakov.Kirilenko@trikset.com>
2018-09-27 12:51:30 +0300

    TravisCI: cache brew cellar

--- .travis/script.sh
+++ .travis/script.sh
@@ -7,18 +7,26 @@ case $TRAVIS_OS_NAME in
      EXECUTOR=
     ;;
   linux)
-      docker run -d -v `pwd`:`pwd` -w `pwd` --name builder trikset/linux-builder Xvfb :0
       EXECUTOR="docker exec builder "
    ;;
   *) exit 1 ;;
 esac
 
 if [ "$VERA" = "true" ]; then $EXECUTOR ./runVera++.sh ; fi
-$EXECUTOR qmake -r CONFIG+=$CONFIG CONFIG+=no-sanitizers QMAKE_CXX='ccache g++' -Wall
-$EXECUTOR make -k -j2
-$EXECUTOR sh -c "cd tests && qmake -r CONFIG+=$CONFIG CONFIG+=no-sanitizers QMAKE_CXX='ccache g++'"
-$EXECUTOR sh -c "cd tests && make -j2"
-$EXECUTOR sh -c "cd bin/x86-$CONFIG && ls"
+$EXECUTOR bash -c "export CCACHE_DIR=$HOME/.ccache/$TRAVIS_OS_NAME-$CONFIG \
+&& export CCACHE_CPP2=yes \
+&& export CCACHE_SLOPPINESS=time_macros \
+&& which g++ \
+&& g++ --version \
+&& which qmake \
+&& qmake -query \
+&& ccache -M 0 \
+&& pkg-config --list-all \
+&&  qmake -r CONFIG+=$CONFIG -Wall \
+&&  make -k -j2 \
+&& pushd tests && qmake -r CONFIG+=$CONFIG && make -j2 && popd \
+&& cd bin/x86-$CONFIG && ls "
+
 $EXECUTOR sh -c "export DISPLAY=:0 && cd bin/x86-$CONFIG && ./trikScriptRunnerTests$SUFFIX"
 $EXECUTOR sh -c "export DISPLAY=:0 && cd bin/x86-$CONFIG && ./trikCommunicatorTests$SUFFIX"
 $EXECUTOR sh -c "export DISPLAY=:0 && cd bin/x86-$CONFIG && ./trikKernelTests$SUFFIX"
