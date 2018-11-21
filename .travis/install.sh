aa709ba45185372a0e8c8cb0ee5875a80be56631
Iakov Kirilenko <jake.kirilenko@gmail.com>
2018-11-22 00:01:53 +0300

    Python-3.6

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,10 +2,13 @@
 set -euxo pipefail
 case $TRAVIS_OS_NAME in
   osx)
-    REQURED_PACKAGES="qt ccache python@2 python@3"
-    for p in $REQURED_PACKAGES ; do
-      rmdir $CELLAR_CACHE_DIR/$p && brew install $p \
-      || brew link --force $p
+    REQURED_PACKAGES="qt ccache https://raw.githubusercontent.com/Homebrew/homebrew-core/f2a764ef944b1080be64bd88dca9a1d80130c558/Formula/python.rb"
+    export HOMEBREW_NO_AUTO_UPDATE=1
+    for pkg in $REQURED_PACKAGES ; do
+      p="${pkg##*/}"
+      p="${p%.*}"
+      rmdir $CELLAR_CACHE_DIR/$p && brew install $pkg \
+      || { brew unlink $p ; brew link --force $p ; }
     done
   ;;
 
