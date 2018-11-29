791c42c59c6e4a23a3f5af05c3a4de7205480434
Iakov 'Jake' Kirilenko <Jake.Kirilenko@gmail.com>
2018-11-29 17:13:42 +0300

    macOS build issue with brew fixed

--- .travis/install.sh
+++ .travis/install.sh
@@ -10,13 +10,14 @@ case $TRAVIS_OS_NAME in
       rmdir $CELLAR_CACHE_DIR/$p && brew install $pkg \
       || { brew unlink $p ; brew link --force $p ; }
     done
-    brew update && brew upgrade $REQUIRED_PACKAGES
+    brew update && brew upgrade $REQUIRED_PACKAGES || true
     # export PYENV_ROOT="$CELLAR_CACHE_DIR/.pyenv"
     export PATH="$(pyenv root)/bin:$PATH"
     eval "$(pyenv init -)"
-    #pyenv update || true
-    pyenv install --list
-    env CFLAGS="-O2 -fPIC" pyenv install -s 3.5.6
+    BEST_AVAILABLE_PYTHON_VERSION=$(pyenv install --list | grep -E '^\s*3\.5\.[0-9]+$' | sort -ruVifb | head -n 1)
+    echo "From pyenv the best matched version is $BEST_AVAILABLE_PYTHON_VERSION"
+    env CFLAGS="-O2 -fPIC" pyenv install -s  $BEST_AVAILABLE_PYTHON_VERSION
+    pyenv global $BEST_AVAILABLE_PYTHON_VERSION
   ;;
 
   linux)
@@ -26,6 +27,6 @@ case $TRAVIS_OS_NAME in
     then
       pip install --user codecov
     fi
-    ;;
+  ;;
   *) exit 1 ;;
 esac
