9f36dfc318a5f24c47bdd9df5b4be53c78bad173
Iakov 'Jake' Kirilenko <Jake.Kirilenko@gmail.com>
2019-01-12 15:51:29 +0300

    Remove '#pragma once' from pch.h

--- vera++/scripts/rules/IncludeGuards.tcl
+++ vera++/scripts/rules/IncludeGuards.tcl
@@ -18,7 +18,7 @@
 
 proc checkGuards { fileName } {
   if {![regexp -nocase {.*.h$} $fileName] } { return }
-  if {[string match "*trikRuntime/pch.h" $fileName] } { return }
+  if {[string match "*/pch.h" $fileName] } { return }
   set lineCount 1 
   set guardFound 0
   foreach line [getAllLines $fileName] {
