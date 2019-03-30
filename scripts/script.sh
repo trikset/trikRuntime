7ee62e50af67565be2a7e7ef6f647acbcb149470
Iakov 'Jake' Kirilenko <Jake.Kirilenko@gmail.com>
2019-03-30 15:04:17 +0300

    Suppress leaks sanitizer LSan for a while

--- scripts/script.sh
+++ scripts/script.sh
@@ -39,7 +39,7 @@ $EXECUTOR bash -lc "{ [ -r /root/.bashrc ] && source /root/.bashrc || true ; } ;
 
 for t in trikKernelTests trikCameraPhotoTests trikCommunicatorTests trikScriptRunnerTests
   do
-    $EXECUTOR env DISPLAY=:0 LSAN_OPTIONS='suppressions=asan.supp fast_unwind_on_malloc=0' sh -c \
+    $EXECUTOR env DISPLAY=:0 ASAN_OPTIONS=detect_leaks=0 LSAN_OPTIONS='detect_leaks=0 suppressions=asan.supp fast_unwind_on_malloc=0' sh -c \
     "cd  $BUILDDIR/bin/x86-$CONFIG && \
      { \
        errCode=0 ; \
