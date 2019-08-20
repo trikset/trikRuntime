29798b8139f10274a8540a1e5c13392e4c32b175
Iakov Kirilenko <iakov@trikset.com>
2019-08-20 13:47:04 +0300

    Log vera++ files list

--- scripts/script.sh
+++ scripts/script.sh
@@ -16,7 +16,7 @@ export EXECUTOR
 if [ "$VERA" = "true" ]; then $EXECUTOR ./runVera++.sh ; fi
 if [ "$VERA" = "true" ]; then git diff --name-only ${TRAVIS_COMMIT_RANGE} \
 	| xargs -r file -i | sed -e "s|\(.*\):.*text/x-c.*|\1|g" -e "/:/d"  \
-	| $EXECUTOR vera++ --error --root vera++ --profile strict ; fi
+	| tee build.log | $EXECUTOR vera++ --error --root vera++ --profile strict ; fi
 if [ "$TRANSLATIONS" = "true" ] ; then $EXECUTOR lupdate trikRuntime.pro && $EXECUTOR scripts/checkStatus.sh ; fi
 
 $EXECUTOR bash -ic "{ [ -r /root/.bashrc ] && source /root/.bashrc || true ; } ; \
