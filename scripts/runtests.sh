adf66c3bb456dbafbb2bf3f92a9e1c2f85b24bd6
Iakov 'Jake' Kirilenko <Jake.Kirilenko@gmail.com>
2019-09-18 16:58:33 +0300

    Tests: ASAN_OPTION `use_sigaltstack=0` helps with ASan crash

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -6,7 +6,7 @@ TRIK_PYTHONPATH=$($EXECUTOR python3 -c 'import sys; import os; print(os.pathsep.
 for t in "$@"
   do
     $EXECUTOR env DISPLAY=:0 \
-    ASAN_OPTIONS="$( [[ $TRAVIS_OS_NAME == linux ]] && echo 'detect_leaks=1:disable_coredump=0:' || :)detect_stack_use_after_return=1:fast_unwind_on_malloc=0:symbolize=1:use_signalstack=0" \
+    ASAN_OPTIONS="$( [[ $TRAVIS_OS_NAME == linux ]] && echo 'detect_leaks=1:disable_coredump=0:' || :)detect_stack_use_after_return=1:fast_unwind_on_malloc=0:symbolize=1:use_sigaltstack=0" \
     LSAN_OPTIONS=suppressions=lsan.supp:fast_unwind_on_malloc=0 \
     TRIK_PYTHONPATH=$TRIK_PYTHONPATH \
     sh -xc \
