d19c802536226d0e72628bc505231094a71e5a1f
Andrey Khodko <andrei.khodko@gmail.com>
2019-10-30 16:34:39 +0300

    Reborn deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
-if [ "$1" ]; then
+#!/bin/bash
+
+set -xuevo pipefail
+
+if [ -n "$1" ]; then
 	ip=$1
 else
 	echo "Usage: deploy.sh <robot ip>"
 	exit 1
 fi
 
-ssh root@$ip 'chmod -x /etc/trik/trikGui.sh && killall trikGui'
+ssh root@$ip " sh -c 'chmod -x /etc/trik/trikGui.sh && killall trikGui'"
 rsync -avz -e ssh . root@$ip:/home/root/trik
-ssh root@$ip 'chmod +x /etc/trik/trikGui.sh && kill -hup 1'
+ssh root@$ip "sh -c 'chmod +x /etc/trik/trikGui.sh && kill -hup 1'"
