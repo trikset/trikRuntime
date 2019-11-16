e342fe92679beacaf85f3b1bb37d092adff82ade
Iakov 'Jake' Kirilenko <Jake.Kirilenko@gmail.com>
2019-11-16 17:32:44 +0300

    Improve Qt installation script

--- docker/install_qt.sh
+++ docker/install_qt.sh
@@ -2,7 +2,8 @@
 set -ueo pipefail
 export TRIK_QT_INSTALL_DIR="${1:-$HOME/Qt}"
 echo "Install Qt to $TRIK_QT_INSTALL_DIR"
-curl -L -o /tmp/qt_installer.run http://download.qt-project.org/official_releases/online_installers/qt-unified-linux-x64-online.run
-chmod +x /tmp/qt_installer.run
-/tmp/qt_installer.run --verbose --platform minimal --no-force-installations --script $(dirname $0)/qt_scriptinstall.qs "${@:2}"
-rm -f /tmp/qt_installer.run
+INSTALLER=/tmp/qt_installer.run
+[ -e "$INSTALLER" ] || curl -L -o "$INSTALLER" http://download.qt-project.org/official_releases/online_installers/qt-unified-linux-x64-online.run \
+&& chmod +x "$INSTALLER"
+"$INSTALLER" --verbose --platform minimal --no-force-installations --script $(dirname $0)/qt_scriptinstall.qs "${@:2}" \
+&& rm -f /tmp/qt_installer.run
