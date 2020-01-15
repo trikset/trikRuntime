d9c48f451682daaec2e6909820fb8dd8d07af83e
Anastasiia Kornilova <kornilova.anastasiia@gmail.com>
2020-01-15 12:00:50 +0300

    Vera satisfaction

--- trikGui/cameraWidget.h
+++ trikGui/cameraWidget.h
@@ -1,3 +1,17 @@
+/* Copyright 2019-2020 CyberTech Labs Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. */
+
 #pragma once
 
 #include <QVBoxLayout>
@@ -12,10 +26,13 @@ class BrickInterface;
 
 namespace trikGui {
 
+// Widget that demonstrates availabilty of the camera and photo from it
 class CameraWidget : public AbstractIndicator
 {
 	Q_OBJECT
 public:
+
+	// Constructor for Camera Widget in testing menu
 	explicit CameraWidget(trikControl::BrickInterface &brick, QWidget *parent = nullptr);
 
 public slots:
