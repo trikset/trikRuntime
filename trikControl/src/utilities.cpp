e2332b49bc31f71322b48b1447f57737aa54097d
Iakov 'Jake' Kirilenko <jake.kirilenko@gmail.com>
2020-03-19 01:11:03 +0300

    Fix memory leak in `show` method

--- trikControl/src/utilities.cpp
+++ trikControl/src/utilities.cpp
@@ -18,43 +18,43 @@
 
 QImage Utilities::imageFromBytes(const QVector<int32_t> &array, int width, int height, const QString &format){
 
-	if (array.empty()) {
-		return QImage();
-	}
-
-	auto fmt = QImage::Format_RGB32;
-	auto *rawData = static_cast<const uchar *>(static_cast<const void *>(array.data()));
-
 	// Reserve maximum possible size to avoid reallocation
-	auto formattedData  = new uchar[(width + 3) * (height + 3) * 3 + 3];
+	uchar *formattedData = nullptr;
 	static auto cleanUp = [](void *p) { if (p) delete [](static_cast<decltype (formattedData)>(p)); };
-	void *cleanUpInfo = nullptr;
 
 	// QImage requires 32-bit aligned scan lines
 	// Helper function to convert data
 	auto copyAligned = [&](int perLine){
+		if (perLine * height > array.size()) {
+			QLOG_WARN() << "imageFromBytes: not enough data";
+			return;
+		}
 		auto scanLineSize = static_cast<int>((static_cast<unsigned>(perLine + 3)) & 0xFFFFFFFC);
-		auto dst = formattedData;
+		auto dst = formattedData = new uchar[scanLineSize * height];
 		for (auto src = array.begin(); src < array.end(); src += perLine) {
 			dst = std::copy(src, src + perLine, dst);
 			dst += scanLineSize - perLine;
 		}
-		rawData = formattedData;
-		cleanUpInfo = formattedData;
 	};
 
+	auto fmt = QImage::Format_Invalid;
+
 	if (!format.compare("rgb32", Qt::CaseInsensitive)) {
-		/* do nothing */
+		fmt = QImage::Format_RGB32;
+		copyAligned(4 * width);
 	} else if (!format.compare("rgb888", Qt::CaseInsensitive)) {
 		fmt = QImage::Format_RGB888;
 		copyAligned(3 * width);
 	} else if (format == "grayscale8") {
 		fmt = QImage::Format_Grayscale8;
-		copyAligned(width);
+		copyAligned(1 * width);
 	} else {
-		QLOG_ERROR() << "Unsupported format " << format;
-		return QImage();
+		QLOG_WARN() << "imageFromBytes: Unsupported format " << format;
 	}
 
-	return QImage(rawData, width, height, fmt, cleanUp, cleanUpInfo);
+	if (!formattedData) {
+		return QImage();
+	} else {
+		return QImage(formattedData, width, height, fmt, cleanUp, formattedData);
+	}
 }
