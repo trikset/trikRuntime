6c7a0fb97401b21156345a9bde011a2c81d83cd0
Iakov 'Jake' Kirilenko <jake.kirilenko@gmail.com>
2020-09-24 23:08:55 +0300

    Run tests with `make check`

--- scripts/script.sh
+++ scripts/script.sh
@@ -18,7 +18,7 @@ if "$VERA" ; then $EXECUTOR ./runVera++.sh ; fi
 
 if [ "$TRANSLATIONS" = "true" ] ; then $EXECUTOR bash -lic 'lupdate trikRuntime.pro' && $EXECUTOR scripts/checkStatus.sh ; fi
 
-$EXECUTOR bash -lixc " \
+$EXECUTOR bash -lic " set -x; \
    export CCACHE_DIR=$HOME/.ccache/$TRAVIS_OS_NAME-$CONFIG \
 && export CCACHE_CPP2=yes \
 && export CCACHE_SLOPPINESS=time_macros \
@@ -32,7 +32,13 @@ $EXECUTOR bash -lixc " \
 && { which python3 && python3 -V || true ; } \
 && { which python && python -V || true ; } \
 &&  cd $BUILDDIR && qmake -r PYTHON3_VERSION_MINOR=\$TRIK_PYTHON3_VERSION_MINOR CONFIG+=$CONFIG -Wall $TRAVIS_BUILD_DIR/trikRuntime.pro $QMAKE_EXTRA \
-&&  make -k -j2 \
-&& cd bin/x86-$CONFIG && ls "
+&& env TRIK_PYTHONPATH=\`python3.\${TRIK_PYTHON3_VERSION_MINOR} -c 'import sys; import os; print(os.pathsep.join(sys.path))'\` \
+    DISPLAY=:0 \
+    PYTHONMALLOC=malloc \
+    ASAN_OPTIONS=disable_coredump=0:detect_stack_use_after_return=1:fast_unwind_on_malloc=0:symbolize=1:use_sigaltstack=0 \
+    LSAN_OPTIONS=suppressions=lsan.supp:fast_unwind_on_malloc=0 \
+    MSAN_OPTIONS=poison_in_dtor=1 \
+    make check -k -j2 \
+&& ls bin/x86-$CONFIG"
 
-exec timeout -k 10s 100s scripts/runtests.sh trikKernelTests trikCameraPhotoTests trikCommunicatorTests trikJsRunnerTests trikPyRunnerTests
+#exec timeout -k 10s 100s scripts/runtests.sh trikKernelTests trikCameraPhotoTests trikCommunicatorTests trikJsRunnerTests trikPyRunnerTests
