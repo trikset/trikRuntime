55fe14c98fbb5b8d8a8529b877eaa6f0708fa39d
Andrei Khodko <andrei.khodko@gmail.com>
2021-07-07 21:07:39 +0300

    Update translations

--- translations/fr/trikScriptRunner_fr.ts
+++ translations/fr/trikScriptRunner_fr.ts
@@ -4,7 +4,7 @@
 <context>
     <name>trikScriptRunner::ScriptEngineWorker</name>
     <message>
-        <location filename="../../trikScriptRunner/src/scriptEngineWorker.cpp" line="+284"/>
+        <location filename="../../trikScriptRunner/src/scriptEngineWorker.cpp" line="+278"/>
         <source>Line %1: %2</source>
         <translation type="unfinished"></translation>
     </message>
