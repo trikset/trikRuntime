b58f84d88fba425b60f73398558fb69517038328
Nikolai Ponomarev <wowasterdev@gmail.com>
2023-08-29 17:57:23 +0300

    Fix git dubious ownership error
    safe.directory option must be configured in global scope for security
    reasons

--- scripts/azure/install_Linux.sh
+++ scripts/azure/install_Linux.sh
@@ -3,6 +3,6 @@ set -euxo pipefail
 
 docker pull trikset/linux-builder
 docker run -d --privileged -v $BUILD_SOURCESDIRECTORY:$BUILD_SOURCESDIRECTORY:rw -w `pwd` --name builder trikset/linux-builder Xvfb :0
-docker exec builder bash -c 'export PATH=/usr/bin:/bin:/usr/sbin:/sbin ; python -V ; python3 -V ; rm -f ~/.bashrc'
+docker exec builder bash -c 'export PATH=/usr/bin:/bin:/usr/sbin:/sbin ; python -V ; python3 -V ; rm -f ~/.bashrc ; git config --global --add safe.directory "*"'
 
 mkdir -p "$BUILDDIR"
