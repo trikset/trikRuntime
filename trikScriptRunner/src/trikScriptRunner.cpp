75b73adb4f72b472352f9fd24996126c40d19ab1
Iakov 'Jake' Kirilenko <jake.kirilenko@gmail.com>
2024-10-05 06:22:40 +0300

    Improved script.wait
    Also some improvements for responsiveness

--- trikScriptRunner/src/trikScriptRunner.cpp
+++ trikScriptRunner/src/trikScriptRunner.cpp
@@ -20,6 +20,7 @@
 
 #include <trikKernel/timeVal.h>
 #include "threading.h"
+#include <QCoreApplication>
 #include <QMetaMethod>
 
 using namespace trikControl;
@@ -156,11 +157,18 @@ void TrikScriptRunner::run(const QString &script, ScriptType stype, const QStrin
 	abortAll(); // FIXME: or fetchRunner(stype)->abort()? or abort(/*last*/)?
 
 	fetchRunner(stype)->run(script, fileName);
+
+	for(QEventLoop l; l.processEvents(); ) {
+	// Enforce events dispatch before return, f.e. handling of stdout messages
+	}
 }
 
 void TrikScriptRunner::runDirectCommand(const QString &command)
 {
 	fetchRunner(mLastRunner)->runDirectCommand(command);
+	for(QEventLoop l; l.processEvents();) {
+	// Enforce events dispatch before return, f.e. handling of stdout messages
+	}
 }
 
 void TrikScriptRunner::abort()
