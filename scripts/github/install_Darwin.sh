df54ff7962d0ca2a88fd90e86eecac6164110007
MinyazevR <89993880+MinyazevR@users.noreply.github.com>
2025-12-17 12:33:39 +0300

    Remove Azure pipelines, add GHA macOS with Python 3.13 (#853)
    * Using PyGILState_Ensure when stopping a Python script results in an infinite wait in newer Python 3.13+
    * Move macOS build from azure to github

--- scripts/github/install_Darwin.sh
+++ scripts/github/install_Darwin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
-REQUIRED_PACKAGES="ccache coreutils python@3.${TRIK_PYTHON3_VERSION_MINOR} pkg-config"
+REQUIRED_PACKAGES="coreutils pkg-config"
 export HOMEBREW_TEMP="$HOME/homebrew.tmp"
 export HOMEBREW_LOGS="$HOMEBREW_TEMP"
 #To turn autoupdate on use `unset HOMEBREW_NO_AUTO_UPDATE` in a sub-shell before `brew install`
@@ -17,15 +17,13 @@ for pkg in $REQUIRED_PACKAGES ; do
     brew install "$pkg" || brew upgrade "$pkg" || brew link --force "$pkg" || echo "Failed to install/upgrade $pkg"
 done
 
-mkdir -p "$BUILDDIR"
-cd "$BUILDDIR"
-TRIK_PYTHON=python3.${TRIK_PYTHON3_VERSION_MINOR}
+TRIK_PYTHON=python${TRIK_PYTHON_VERSION}
 "$TRIK_PYTHON" -m pip install -U pip
 "$TRIK_PYTHON" -m pip install aqtinstall
-"$TRIK_PYTHON" -m aqt install-qt -m qtscript -O "$HOME/Qt" mac desktop "${TRIK_QT_VERSION}" \
-            --archives qtbase qtmultimedia qtsvg qttools qtserialport qtimageformats
-# Force SDK version compatible with Qt 5.12
-system_profiler -json SPDeveloperToolsDataType | jq '.SPDeveloperToolsDataType[].spdevtools_path' || :
-sudo xcode-select -s /Applications/Xcode_14.3.app/Contents/Developer
+"$TRIK_PYTHON" -m aqt install-qt -m qtscript -O "$HOME/Qt" mac desktop "${TRIK_QT_VERSION}"
+QT_DIR=$(ls -dv "$HOME"/Qt/${TRIK_QT_VERSION}*/*/bin | head -n 1)
+echo "$QT_DIR" >> $GITHUB_PATH
+            
+sudo xcode-select -s /Applications/Xcode_15.3.app/Contents/Developer
 xcodebuild -showsdks
 xcrun -sdk macosx --show-sdk-path
